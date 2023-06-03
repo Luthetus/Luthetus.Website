16a3dfcde56f4da43438086ba62054609bab1547
Hunter Freeman <luthetus@gmail.com>
2023-06-03 15:01:06 -0400

    goto-definition across files works -- but only once. Trying again gives duplicate key error, I'm gonna look into it.

--- Source/Lib/Luthetus.Website.RazorLib/Repl/TextEditor/ReplTextEditorGroupDisplay.razor.cs
+++ Source/Lib/Luthetus.Website.RazorLib/Repl/TextEditor/ReplTextEditorGroupDisplay.razor.cs
@@ -1,6 +1,14 @@
+using Fluxor;
 using Luthetus.Common.RazorLib.Dimensions;
 using Luthetus.Common.RazorLib.Store.ApplicationOptions;
+using Luthetus.Ide.ClassLib.CompilerServices.Common.Symbols;
+using Luthetus.Ide.ClassLib.FileSystem.Classes.FilePath;
+using Luthetus.Ide.ClassLib.FileSystem.Interfaces;
+using Luthetus.Ide.ClassLib.Store.EditorCase;
+using Luthetus.Ide.ClassLib.TreeViewImplementations;
+using Luthetus.TextEditor.RazorLib;
 using Luthetus.TextEditor.RazorLib.Group;
+using Luthetus.TextEditor.RazorLib.Semantics;
 using Luthetus.Website.RazorLib.Store.ReplCase;
 using Microsoft.AspNetCore.Components;
 
@@ -8,6 +16,13 @@ namespace Luthetus.Website.RazorLib.Repl.TextEditor;
 
 public partial class ReplTextEditorGroupDisplay : ComponentBase
 {
+    [Inject]
+    private IDispatcher Dispatcher { get; set; } = null!;
+    [Inject]
+    private ITextEditorService TextEditorService { get; set; } = null!;
+    [Inject]
+    private IEnvironmentProvider EnvironmentProvider { get; set; } = null!;
+
     [CascadingParameter, EditorRequired]
     public ReplState ReplState { get; set; } = null!;
     [CascadingParameter, EditorRequired]
@@ -17,4 +32,35 @@ public partial class ReplTextEditorGroupDisplay : ComponentBase
 
     [Parameter, EditorRequired]
     public ElementDimensions ElementDimensions { get; set; } = null!;
+
+    private void HandleGotoDefinitionWithinDifferentFileAction(TextEditorSymbolDefinition textEditorSymbolDefinition)
+    {
+        var model = TextEditorService.Model.FindOrDefaultByResourceUri(
+            textEditorSymbolDefinition.ResourceUri);
+
+        if (model is null)
+            return;
+
+        var viewModels = TextEditorService.Model.GetViewModelsOrEmpty(model.ModelKey);
+
+        if (!viewModels.Any())
+            return;
+
+        var viewModel = viewModels[0];
+
+        var rowInformation = model.FindRowInformation(
+            textEditorSymbolDefinition.PositionIndex);
+
+        viewModel.PrimaryCursor.IndexCoordinates =
+            (rowInformation.rowIndex,
+                textEditorSymbolDefinition.PositionIndex - rowInformation.rowStartPositionIndex);
+
+        Dispatcher.Dispatch(new EditorState.OpenInEditorAction(
+            new AbsoluteFilePath(
+                textEditorSymbolDefinition.ResourceUri.Value,
+                false,
+                EnvironmentProvider),
+            true,
+            ReplTextEditorGroupKey));
+    }
 }
