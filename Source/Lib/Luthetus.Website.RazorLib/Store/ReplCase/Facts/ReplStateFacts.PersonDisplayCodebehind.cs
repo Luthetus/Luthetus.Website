7e4d973947176bbb54190780a24c26af50a44f59
Hunter Freeman <luthetus@gmail.com>
2023-06-03 23:16:51 -0400

    BoundAttributeNode

---
+++ Source/Lib/Luthetus.Website.RazorLib/Store/ReplCase/Facts/ReplStateFacts.PersonDisplayCodebehind.cs
@@ -0,0 +1,18 @@
+﻿namespace Luthetus.Website.RazorLib.Store.ReplCase.Facts;
+
+public partial class ReplStateFacts
+{
+    public static readonly string PERSON_DISPLAY_CODEBEHIND_ABSOLUTE_FILE_PATH = @"/BlazorWasmApp/PersonCase/PersonDisplay.razor.cs";
+
+    public static readonly string PERSON_DISPLAY_CODEBEHIND_CONTENTS = @"using Microsoft.AspNetCore.Components;
+
+namespace BlazorWasmApp.Components;
+
+public partial class PersonDisplay : ComponentBase
+{
+	[Parameter, EditorRequired]
+	public IPersonModel PersonModel { get; set; }
+}
+";
+}
+
---
+++ Source/Lib/Luthetus.Website.RazorLib/Store/ReplCase/Facts/ReplStateFacts.PersonDisplayMarkup.cs
@@ -0,0 +1,11 @@
+﻿namespace Luthetus.Website.RazorLib.Store.ReplCase.Facts;
+
+public partial class ReplStateFacts
+{
+    public static readonly string PERSON_DISPLAY_MARKUP_ABSOLUTE_FILE_PATH = @"/BlazorWasmApp/PersonCase/PersonDisplay.razor";
+
+    public static readonly string PERSON_DISPLAY_MARKUP_CONTENTS = @"<div class=""bwa_person"">
+	@PersonModel.DisplayName
+</div>";
+}
+
